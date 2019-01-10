e73393d45f984a1385928e48df7fdaf216c0dfd9
Frankie Rodriguez <rfrankie04@gmail.com>
2019-01-09 22:24:35 -0500

    template created

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './Resources/Photos/Logos/OVOKNUPES.png';
 import './Styles/App.css';
 import Button from "./components/button";
 import Slideshow from "./components/slideshow";
+import * as Scroll from "react-scroll";
 
 class App extends Component {
     render() {
@@ -10,9 +11,13 @@ class App extends Component {
             <div>
                 <div className="header">
                     <img className="header-logo" src={logo} width="300" height="200" alt={""}/>
-                    <a href="">About</a>
-                    <a href="">Contact</a>
-                    <a href="">Upcoming Events</a>
+                    <ul>
+                        <Scroll.Link to="history" smooth={true} duration={500}>Chapter History</Scroll.Link>
+                        <Scroll.Link to="boty" smooth={true} duration={500}>Brothers on the yard</Scroll.Link>
+                        <Scroll.Link to="eventCalendar" smooth={true} duration={500}>Event Calendar</Scroll.Link>
+                        <Scroll.Link to="gallery" smooth={true} duration={500}>Gallery</Scroll.Link>
+                        <Scroll.Link to="contact" smooth={true} duration={500}>Contact us</Scroll.Link>
+                    </ul>
                     <div className="header-right">
                         <Button className="header-admin-button"/>
                     </div>
@@ -20,6 +25,149 @@ class App extends Component {
                 <div>
                     <Slideshow/>
                 </div>
+                <hr/>
+                <div id="history">
+                    <h1>About Our Chapter</h1>
+                </div>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <hr/>
+                <div id="boty">
+                    <h1>Brothers on the yard</h1>
+                </div>
+
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <hr/>
+                <div id="eventCalendar">
+                    <h1>Event calendar</h1>
+                </div>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+
+                <hr/>
+                <div id="gallery">
+                    <h1>Gallery</h1>
+                </div>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+
+                <hr/>
+                <div id="contact">
+                    <h1>Contact Us</h1>
+                </div>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
             </div>
 
         );
--- src/components/slideshow.js
+++ src/components/slideshow.js
@@ -1,21 +1,43 @@
-import React, {Component} from 'react';
+import React from 'react';
 import '../Styles/Slideshow.css';
 import AllOK from '../Resources/Photos/SlideShow/AllOk.png';
 import ChrisTrack from '../Resources/Photos/SlideShow/ChrisTrack.png';
 import OkAtNj from '../Resources/Photos/SlideShow/OkAtNJ.png';
+import { Fade } from 'react-slideshow-image';
 
-class Slideshow extends Component {
-    render() {
-        return (
-            <div id="slideshow">
-                <div className="slide-wrapper">
-                    <div className="slide"><img src={AllOK}/></div>
-                    <div className="slide"><img src={ChrisTrack}/></div>
-                    <div className="slide"><img src={OkAtNj}/></div>
-                </div>
-            </div>
-        );
-    };
+const fadeImages = [
+    AllOK,
+    ChrisTrack,
+    OkAtNj
+];
+
+const fadeProperties = {
+    duration: 8000,
+    transitionDuration: 1000,
 };
 
+const Slideshow = () => {
+    return (
+        <Fade {...fadeProperties}>
+            <div className="slide">
+                <div>
+                    <img src={fadeImages[0]} alt={"img not available"}/>
+                </div>
+                <h2>OK Chapter Picture 2017</h2>
+            </div>
+            <div className="slide">
+                <div>
+                    <img src={fadeImages[1]} alt={"img not available"}/>
+                </div>
+                <h2>SP17 Brother Chris Antione</h2>
+            </div>
+            <div className="slide">
+                <div>
+                    <img src={fadeImages[2]} alt={"img not available"}/>
+                </div>
+                <h2>OK Nupes at Nupe Jersey</h2>
+            </div>
+        </Fade>
+    );
+};
 export default Slideshow;
