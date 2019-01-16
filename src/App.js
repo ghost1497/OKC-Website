9caf0b2e5fce7bc80b765718c03263cfe6a3b428
Rodriguez <frodrig6@cscinfo.com>
2019-01-15 19:54:38 -0500

    added google calendar iframe

--- src/App.js
+++ src/App.js
@@ -16,11 +16,16 @@ class App extends Component {
                 <div className="header">
                     <img src={logo} width="300" height="200" alt={""}/>
                     <ul className="linkContainer">
-                        <Scroll.Link className="linkChild" to="history" smooth={true} duration={500}>Chapter History</Scroll.Link>
-                        <Scroll.Link className="linkChild" to="boty" smooth={true} duration={500}>Brothers on the yard</Scroll.Link>
-                        <Scroll.Link className="linkChild" to="eventCalendar" smooth={true} duration={500}>Event Calendar</Scroll.Link>
-                        <Scroll.Link className="linkChild" to="gallery" smooth={true} duration={500}>Gallery</Scroll.Link>
-                        <Scroll.Link className="linkChild" to="contact" smooth={true} duration={500}>Contact us</Scroll.Link>
+                        <Scroll.Link className="linkChild" to="history" smooth={true} duration={500}>Chapter
+                            History</Scroll.Link>
+                        <Scroll.Link className="linkChild" to="boty" smooth={true} duration={500}>Brothers on the
+                            yard</Scroll.Link>
+                        <Scroll.Link className="linkChild" to="eventCalendar" smooth={true} duration={500}>Event
+                            Calendar</Scroll.Link>
+                        <Scroll.Link className="linkChild" to="gallery" smooth={true}
+                                     duration={500}>Gallery</Scroll.Link>
+                        <Scroll.Link className="linkChild" to="contact" smooth={true} duration={500}>Contact
+                            us</Scroll.Link>
                     </ul>
                     <div className="header-right">
                         <AdminButton className="header-admin-button"/>
@@ -62,15 +67,16 @@ class App extends Component {
                     <BrotherOnYard/>
                 </div>
                 <hr/>
-                <div id="eventCalendar">
-                    <h1>Event calendar</h1>
-                <Calendar/>
+                <div id="eventCalendar" className="calendar">
+                    <h1>Event Calendar</h1>
+                    <iframe
+                        src="https://calendar.google.com/calendar/embed?showPrint=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=qnvd4h9e45ofncs5fdrmq414po%40group.calendar.google.com&amp;color=%23711616&amp;ctz=America%2FNew_York"
+                        width="800" height="600" frameBorder="0" scrolling="no"></iframe>
                 </div>
-
                 <hr/>
                 <div id="gallery">
                     <h1>Gallery</h1>
-                        <Photo/>
+                    <Photo/>
                 </div>
 
                 <hr/>
