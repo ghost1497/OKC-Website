d8903e62e7adae2630a01f2b5f2f49464b1f0d8d
Frankie Rodriguez <rfrankie04@gmail.com>
2019-02-18 22:57:45 -0500

    took some pics away from gallery

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -11,18 +11,14 @@ const PHOTO_SET = [
     { src: 'http://drive.google.com/uc?export=view&id=1OgfrdZ3KO_GtoDFOMmGTIfEz6CUuseM7', width: 3.5, height: 3 },
     { src: 'http://drive.google.com/uc?export=view&id=1L898Lj9EOQ3AaeUXyEUAWBXiagjHbn5s', width: 4, height: 2.5 },
     { src: OkAtNj, width: 4.5, height: 3 },
-    { src: 'http://drive.google.com/uc?export=view&id=1FJhuVVjiow1Y1WSmb7_7s2wjQkYcpjr_', width: 6, height: 3 },
     { src: AllOK, width: 4, height: 3 },
     { src: 'http://drive.google.com/uc?export=view&id=1BRhzjZRMB5QsjuiuZjek5rF8VFYeRSEO', width: 4, height: 3 },
     { src: 'http://drive.google.com/uc?export=view&id=1Xe4GOd3fEXbE7xD-Nrosb9IVwrTgHLBJ', width: 3.5, height: 4.5 },
     { src: 'http://drive.google.com/uc?export=view&id=1i06HMThyJsf9jjWWTlb4-Z5YkYv_1531', width: 3, height: 4 },
-    { src: 'http://drive.google.com/uc?export=view&id=1AYCacePBlZ5PJbFM3teXGPPE8xDOQpJE', width: 4, height: 3 },
     { src: 'http://drive.google.com/uc?export=view&id=1VamwZ2BNtmQSHv0lN_cIVc_htf7OStqC', width: 5, height: 3 },
     { src: 'http://drive.google.com/uc?export=view&id=11UkROElUWzNT2cY15HsXqW4_5C4M_MvS', width: 5, height: 3 },
-    { src: 'http://drive.google.com/uc?export=view&id=18xskoAlVRe5XqZRAd6Kk5Ls0Dw9O-VbE', width: 4.5, height: 3 },
     { src: 'http://drive.google.com/uc?export=view&id=1bXbdDlmzpW9RqPvLKKgiiDhVJccoFoK-', width: 5, height: 3 },
-    { src: 'http://drive.google.com/uc?export=view&id=1CVS5E43lnrUOTu5XBaVfbmYQdjjO_eya', width: 5, height: 3.5 },
-    { src: 'http://drive.google.com/uc?export=view&id=1DxYi_SgtYG1TPZBfcWJJbatunsHhUM-a', width: 5, height: 3 }
+    { src: 'http://drive.google.com/uc?export=view&id=1CVS5E43lnrUOTu5XBaVfbmYQdjjO_eya', width: 5, height: 3.5 }
 ];
 
 class Photo extends React.Component {
